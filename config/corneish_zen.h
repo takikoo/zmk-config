db642478c3a29f0363c18186b2555450f5293fc5
takikoo <takiko@protonmail.com>
2023-12-19 22:51:21 +0100

    Minor refactor

--- config/corneish_zen.h
+++ config/corneish_zen.h
@@ -1,4 +1,5 @@
 // clang-format off
+
 // LAYERS
 #define QWERTY 0
 #define GAME 1
@@ -47,13 +48,4 @@
 #define TAB_PREV &kp LC(PG_UP)
 #define TAB_NEXT &kp LC(PG_DN)
 
-// HOMEROW
-#define HM_A &hm_aodia LGUI A
-#define HM_S &hm_sl LALT S
-#define HM_D &hm_dk LCTRL D
-#define HM_F &hm_fj LSHFT F
-#define HM_ODIA &hm_aodia LGUI SE_ODIA
-#define HM_L &hm_sl LALT L
-#define HM_K &hm_dk LCTRL K
-#define HM_J &hm_fj LSHFT J
 // clang-format on
