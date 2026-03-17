4d6a6fe2b75a60b7aac824a996bc2728bb3b04bb
takikoo <13720392+takikoo@users.noreply.github.com>
2026-03-17 01:02:57 +0100

    fix(config): remove undefined IDLE_OFF_TIMEOUT and fix label redefinitions
    - Remove CONFIG_ZMK_IDLE_OFF_TIMEOUT which is not in standard ZMK
    - Rename THUMBS to TOTEM_THUMBS in homerow_mods-totem.h to avoid conflict with zmk-helpers
    - Clean up redundant Kconfig settings in totem.conf

--- config/homerow_mods-totem.h
+++ config/homerow_mods-totem.h
@@ -1,7 +1,7 @@
 /* Homerow mods */
 #define KEYS_L LT0 LT1 LT2 LT3 LT4 LM0 LM1 LM2 LM3 LM4 LB0 LB1 LB2 LB3 LB4 LB5  // left-hand keys
 #define KEYS_R RT0 RT1 RT2 RT3 RT4 RM0 RM1 RM2 RM3 RM4 RB0 RB1 RB2 RB3 RB4 RB5  // right-hand keys
-#define THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                          // thumb keys
+#define TOTEM_THUMBS LH2 LH1 LH0 RH0 RH1 RH2                                          // thumb keys
 
 #define QUICK_TAP_MS 175
 
@@ -12,7 +12,7 @@ ZMK_BEHAVIOR(hml, hold_tap,
     quick-tap-ms = <QUICK_TAP_MS>;                // repeat on tap-into-hold
     require-prior-idle-ms = <150>;
     bindings = <&kp>, <&kp>;
-    hold-trigger-key-positions = <KEYS_R THUMBS>;
+    hold-trigger-key-positions = <KEYS_R TOTEM_THUMBS>;
     hold-trigger-on-release;             // delay positional check until key-release
 )
 
@@ -23,7 +23,7 @@ ZMK_BEHAVIOR(hmr, hold_tap,
     quick-tap-ms = <QUICK_TAP_MS>;                // repeat on tap-into-hold
     require-prior-idle-ms = <150>;
     bindings = <&kp>, <&kp>;
-    hold-trigger-key-positions = <KEYS_L THUMBS>;
+    hold-trigger-key-positions = <KEYS_L TOTEM_THUMBS>;
     hold-trigger-on-release;             // delay positional check until key-release
 )
 
@@ -34,7 +34,7 @@ ZMK_BEHAVIOR(hml_repeat, hold_tap,
     quick-tap-ms = <QUICK_TAP_MS>;                // repeat on tap-into-hold
     require-prior-idle-ms = <150>;
     bindings = <&kp>, <&key_repeat>;
-    hold-trigger-key-positions = <KEYS_R THUMBS>;
+    hold-trigger-key-positions = <KEYS_R TOTEM_THUMBS>;
     hold-trigger-on-release;             // delay positional check until key-release
 )
 
